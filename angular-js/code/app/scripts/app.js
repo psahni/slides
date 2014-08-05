2461bd865e4551878f06e58d0fb301c035bb22fe
Prashant Sahni <prashant.sahni5@gmail.com>
2014-08-05 19:45:27 +0530

    examples addeed

--- angular-js/code/app/scripts/app.js
+++ angular-js/code/app/scripts/app.js
@@ -12,9 +12,31 @@ var app = angular
   .module('codeApp', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider){
-  $routeProvider.when("/", {templateUrl: 'views/main.html', controller:'MainCtrl'});
-  $routeProvider.when('/example1', {templateUrl: 'views/example.html', controller: 'Examples'});
-
+    $routeProvider.when("/", {templateUrl: 'views/main.html', controller:'MainCtrl'});
+    $routeProvider.when('/example1', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example2', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example3', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example4', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example5', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example6', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example7', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example8', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example9', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example10', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example11', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example12', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example13', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example14', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example15', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example16', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example17', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example18', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example19', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example20', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example21', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example22', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example23', {templateUrl: 'views/example.html', controller: 'Examples'});
+    $routeProvider.when('/example24', {templateUrl: 'views/example.html', controller: 'Examples'});
 }]);
 
 
